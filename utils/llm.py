7f72f1c404439daa26a034426175edbed2b13905
Marvin von Rappard <marvin@von-rappard.ch>
2024-02-22 14:44:49 +0100

    Update random.choices range in get_most_engaging_titles function

--- utils/llm.py
+++ utils/llm.py
@@ -147,7 +147,7 @@ Respond with JSON in the following format:
         titles[i] for i in most_engaging_titles if i < len(titles) and titles[i]
     ]
 
-    return random.choices(sorted_titles[: max(len(titles) // 2, n)], k=n)
+    return random.choices(sorted_titles[: max(len(titles) // 4, n)], k=n)
 
 
 def get_best_title(titles: List[str]) -> str:
